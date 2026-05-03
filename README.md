d8be4ea9f0a2b5bdc8d4c6e70e3ad896e3de813e
Pro Bandey <195909043+Pro-Bandey@users.noreply.github.com>
2026-05-04 00:00:00 +0500

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# Stylish-QR-Code
+Generate branded QR code online, you can add image and color to your QR code. Use the Stylish QR Code to generate custimize stylish QR codes.
---
+++ sw.js
@@ -0,0 +1,3 @@
+
+  import { precacheAndRoute } from 'workbox-precaching/precacheAndRoute';
+  precacheAndRoute([{"revision":"af3f011b79e0d19f3d9b4a2c7c00d393","url":"index.css"},{"revision":"12595377f4413e935e6d0ad839324638","url":"index.html"},{"revision":"c7154bd7be801bbb2c26ee76ded827f4","url":"index.js"},{"revision":"377cff0ffdc862c3b8e98ccfbd07c928","url":"mrc/index.html"},{"revision":"02b9cbf9629b43c3a900fe80eb3d78eb","url":"pb/index.html"}]);
